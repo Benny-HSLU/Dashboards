20b5343faff46e0c6c1175da222c900def0577c8
Benny-HSLU <benjamin.hornung@stud.hslu.ch>
2026-06-21 13:57:14 +0200

    shkjkdbawjk

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Gebäude Monitoring", layout="wide")
 st.title("Willkommen beim Gebäude-Monitoring-System")
 st.markdown("""
 Wählen Sie links in der Sidebar das passende Dashboard aus:
-- **Laien-Dashboard**: Fokus auf Wohnkomfort (Temperatur & Feuchtigkeit).
-- **Experten-Dashboard**: Technische Analyse der Heizkurve und Gebäudeperformance.
+- **Laien-Dashboard**: Schauen sie die Temperatur- und Feuchtigkeitswerte spezifischer Wohnungen nach. Und vergleichen sie sie.
+- **Experten-Dashboard**: Technische Analyse der Heizkurve und Trendanalyse.
+- **Debug-Dashboard**: Jede Datenquelle einzeln um Anomalien und Incosistenzen zu finden.
 """)
